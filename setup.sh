19f5b995c5381cad53ad3687bdebb698c4f13faa
aelmalki <elmalkiahmed02@gmail.com>
2024-04-30 18:03:45 +0100

    modified permissions

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 image_path="$HOME/z01-config/Background.jpg"
 git clone https://https://github.com/Ahmedelmalki/z01-settings ~/z01-config && cd ~/z01-config/ && mv .p10k.zsh ~/.p10k.zsh && mv .zshrc ~/.zshrc && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+zsh git_setup.sh
 gsettings set org.gnome.desktop.background picture-uri-dark "file://${image_path}" && gsettings set org.gnome.desktop.background picture-uri "file://${image_path}"
