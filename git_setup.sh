d53a84eabdeb764c0e241ba2671c12e16e940542
Ahmedelmalki <167434111+Ahmedelmalki@users.noreply.github.com>
2024-04-30 17:54:41 +0100

    Add files via upload

---
+++ git_setup.sh
@@ -0,0 +1,5 @@
+NAME="yhajjaoui"
+EMAIL="yousefhajjaoui67@gmail.com"
+git config --global credential.helper store
+git config --global user.email $EMAIL
+git config --global user.name $NAME
---
+++ setup.sh
@@ -0,0 +1,3 @@
+image_path="$HOME/z01-config/Background.jpg"
+git clone https://https://github.com/Ahmedelmalki/z01-settings ~/z01-config && cd ~/z01-config/ && mv .p10k.zsh ~/.p10k.zsh && mv .zshrc ~/.zshrc && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+gsettings set org.gnome.desktop.background picture-uri-dark "file://${image_path}" && gsettings set org.gnome.desktop.background picture-uri "file://${image_path}"
